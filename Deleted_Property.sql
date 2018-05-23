338d36f2d42d94cf346d7bef60326f1b054dfcb9
Nicholas Maietta <nick@commnetivity.com>
2018-05-22 23:30:12 -0700

    Never deleted a file before via Git. Weird.
    	deleted:    ActiveAgent.sql
    	deleted:    ActiveOffice.sql
    	deleted:    Agent.sql
    	deleted:    Contact.sql
    	deleted:    Deleted_Agent.sql
    	deleted:    Deleted_Office.sql
    	deleted:    Deleted_Property.sql
    	deleted:    Hotsheet_CI_3.sql
    	deleted:    Hotsheet_LD_2.sql
    	deleted:    Hotsheet_MF_4.sql
    	deleted:    Hotsheet_RE_1.sql
    	deleted:    Media_CI_3.sql
    	deleted:    Media_LD_2.sql
    	deleted:    Media_MF_4.sql
    	deleted:    Media_RE_1.sql
    	deleted:    Office.sql
    	deleted:    OpenHouse_CI_3.sql
    	deleted:    OpenHouse_LD_2.sql
    	deleted:    OpenHouse_MF_4.sql
    	deleted:    OpenHouse_RE_1.sql
    	deleted:    PartialProperty_CI_3.sql
    	deleted:    PartialProperty_LD_2.sql
    	deleted:    PartialProperty_MF_4.sql
    	deleted:    PartialProperty_RE_1.sql
    	deleted:    Property_CI_3.sql
    	deleted:    Property_LD_2.sql
    	deleted:    Property_MF_4.sql
    	deleted:    Property_RE_1.sql
    	deleted:    Tour_CI_3.sql
    	deleted:    Tour_LD_2.sql
    	deleted:    Tour_MF_4.sql
    	deleted:    Tour_RE_1.sql

--- Deleted_Property.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE Deleted_Property (
-  `X_Deleted_ID` INT(10) COMMENT 'Deleted ID',
-  `X_Deleted_Date` DATE default '0000-00-00' not null COMMENT 'Deleted Date',
-  `X_DisplayId` VARCHAR(15) COMMENT 'MLS #',
-  PRIMARY KEY(`X_Deleted_ID`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
